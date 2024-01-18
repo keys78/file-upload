336934c5f0f7589e4bc4758c482e25cbefd3882b
Emmanuel Onyeyaforo <rahzy24@gmail.com>
2024-01-18 09:49:38 +0100

    feat: project completed

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-      await mongoose.connect(process.env.DB_URI_LOCAL)
-    //   await mongoose.connect(process.env.MONGO_URI)
+      // await mongoose.connect(process.env.DB_URI_LOCAL)
+      await mongoose.connect(process.env.MONGO_URI)
       console.log("MongoDB connected");
     } catch (error) {
       console.error("MongoDB connection error:", error);
